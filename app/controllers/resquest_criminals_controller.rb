0e4fb2f7c2698ccdcb017ad1b03674ddcd6b07c1
Rodrigo <rodrigo19962010@live.com>
2017-05-20 14:12:41 -0300

    All

--- app/controllers/resquest_criminals_controller.rb
+++ app/controllers/resquest_criminals_controller.rb
@@ -36,6 +36,7 @@ class ResquestCriminalsController < ApplicationController
     @resquest_criminal = ResquestCriminal.new(resquest_criminal_params)
     
     # Nessa linha a requisição atual é setada com o ID do usuário atual
+    
     @resquest_criminal.user_id = current_user.id
     @resquest_criminal.district_resquest = current_user.district
     respond_to do |format|
@@ -88,6 +89,6 @@ class ResquestCriminalsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def resquest_criminal_params
-      params.require(:resquest_criminal).permit(:district_resquest_id, :district_send_id, :user_id, :resquest_type_id, :person_id)
+      params.require(:resquest_criminal).permit(:district_resquest_id, :district_send_id, :user_id, :resquest_type_id, :person_id, :pdf)
     end
 end
